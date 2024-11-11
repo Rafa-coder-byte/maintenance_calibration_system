8f1e9f96fc17988468a748a072619345d954b28d
Rafa-coder-byte <rafaelolivareyes.321.xxx@gmail.com>
2024-11-11 04:46:13 +0300

    Agregar archivos de proyecto.

---
+++ maintenance_calibration_system.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace maintenance_calibration_system.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
